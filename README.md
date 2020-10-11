e0a448432550544ca5eab05fc1ab2c8f25760567
himanshu9345 <hbp53@njit.edu>
2020-10-11 18:22:26 -0400

    Added publisher code

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # rabbitmq-job-manager
+
 Learning Rabbitmq
+
+Initializing Rabbitmq Server
+
+- docker run --name rabbitmq -p 5672:5672 rabbitmq
---
+++ publisher.js
@@ -0,0 +1,19 @@
+const amqp = require("amqplib");
+
+connect();
+async function connect() {
+  const msgToSend = { number: 19 };
+
+  try {
+    // connecting to rabbitmq server
+    const connection = await amqp.connect("amqp://localhost:5672");
+    // creating channels
+    const channel = await connection.createChannel();
+    // create a channel queue
+    const result = await channel.assertQueue("myjobsqueue");
+    channel.sendToQueue("myjobsqueue", Buffer.from(JSON.stringify(msgToSend)));
+    console.log("Job sent to myjobsqueue");
+  } catch (error) {
+    console.log(error);
+  }
+}
